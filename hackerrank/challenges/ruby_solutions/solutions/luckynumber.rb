6dac95cc18ce5603bb2474ee32de31f33a84475b
Enkhbold Tsagaach <etsagaach@medexpert.com>
2013-10-28 17:37:11 -0700

    gitignore file added

--- hackerrank/challenges/ruby_solutions/solutions/luckynumber.rb
+++ hackerrank/challenges/ruby_solutions/solutions/luckynumber.rb
@@ -5,19 +5,7 @@
 @primes_array = Array.new(@MAX_SQUARE + 1,true)
 @primes = {}
 
-@dyn_table = []
-(@MAX_LENGTH+1).times do
-  tmp = []
-  (@MAX_SUM+1).times do
-    tmp2 = []
-    (@MAX_SQUARE+1).times do
-      tmp2 << 0
-    end
-    tmp << tmp2
-  end
-  @dyn_table << tmp
-end
-
+@dyn_table = Array.new(@MAX_LENGTH+1) { Array.new(@MAX_SUM+1) { Array.new(@MAX_SQUARE+1,0) } }
 
 def fillPrime
   @primes_array[0] = @primes_array[1] = false;
@@ -42,7 +30,7 @@ end
 
 def fillDynTable
   @dyn_table[0][0][0] = 1
-  (0..@MAX_LENGTH-1).each do |i|
+  (0..@MAX_LENGTH-2).each do |i|
     (0..i*9).each do |j|
       (0..i*9*9).each do |k|
         (0..9).each do |l|
@@ -54,59 +42,106 @@ def fillDynTable
 end
 
 def split(number)
+
+  if number == 0
+    return [0]
+  end
+
   digits = []
   while (number > 0)
     d = number % 10
     digits << d
     number = number / 10
   end
+  digits
 
-  digits.reverse!
 end
 
 def calc(max)
   cnt = 0
   digits = split(max)
+  digits.reverse!
+
+  @primes.each_key do |pr|
+    @primes.each_key do |pr2|
+      if pr2 < pr
+        next
+      end
+      sum = 0;
+      sumSquare = 0;
+
+      (0..digits.length-2).each do |idx|
+        d = digits[idx]
+        (0..d-1).each do |i|
+          cnt += @dyn_table[digits.length - idx - 1][pr-sum-i][pr2-sumSquare-i*i] if @dyn_table[digits.length - idx - 1] &&
+              @dyn_table[digits.length - idx - 1][pr-sum-i] &&
+              @dyn_table[digits.length - idx - 1][pr-sum-i][pr2-sumSquare-i*i]
+        end
+        sum += d
+        sumSquare += d*d
+      end
+    end
+  end
+
   sum = 0;
   sumSquare = 0;
-  digits.each_with_index do |d, idx|
-    if idx == digits.length - 1
-      (0..d).each do |i|
-        cnt += 1 if @primes[i + sum] == true && @primes[i*i + sumSquare] == true
-      end
-    else
-      @primes.each_key do |pr|
-        @primes.each_key do |pr2|
-          if pr2 < pr
-            next
-          end
-          (0..d-1).each do |i|
-            cnt += @dyn_table[digits.length - idx - 1][pr-sum-i][pr2-sumSquare-i*i] if @dyn_table[digits.length - idx - 1] &&
-                @dyn_table[digits.length - idx - 1][pr-sum-i] &&
-                @dyn_table[digits.length - idx - 1][pr-sum-i][pr2-sumSquare-i*i]
-          end
-        end
-
-      end
-      sum += d
-      sumSquare += d*d
-    end
-
+  (0..digits.length-2).each do |idx|
+    d = digits[idx]
+    sum += d
+    sumSquare += d*d
   end
+
+  d = digits[digits.length-1]
+  (0..d).each do |i|
+    cnt += 1 if @primes[i + sum] == true && @primes[i*i + sumSquare] == true
+  end
+
   cnt
 end
 
-start = Time.now
+def calc2(max)
+  result = 0
+  digits = split(max)
+
+  sum = 0
+  sq_sum = 0
+  (digits.length-1).downto(0).each do |i|
+    step_result = 0
+    (0..digits[i]-1).each do |l|
+      (0..9*i).each do |j|
+        (0..9*9*i).each do |k|
+          if(@primes[j+l+sum] && @primes[k+l*l + sq_sum])
+            step_result += @dyn_table[i][j][k]
+          end
+        end
+      end
+    end
+    result += step_result
+    sum += digits[i]
+    sq_sum += digits[i]*digits[i]
+  end
+
+  if (@primes[sum] && @primes[sq_sum])
+    result += 1
+  end
+
+  result
+end
+
 fillPrime()
-puts "Duration: #{Time.now - start} seconds"
 
-start = Time.now
 fillDynTable()
-puts "Duration: #{Time.now - start} seconds"
 
-
-t = gets.strip.to_i
-t.times do
-  input = STDIN.gets.chomp.split(' ')
+text=File.open('C:\projects\algorithm\hackerrank\challenges\ruby_solutions\input2.txt').read
+text.gsub!(/\r\n?/, "\n")
+text.each_line do |line|
+  input = line.split(' ')
+  input[1] = "999999999999999999" if input[1] == "1000000000000000000"
   puts (calc(input[1].to_i) - calc(input[0].to_i-1))
 end
+
+#t = gets.strip.to_i
+#t.times do
+#  input = STDIN.gets.chomp.split(' ')
+#  puts (calc2(input[1].to_i) - calc2(input[0].to_i-1))
+#end
