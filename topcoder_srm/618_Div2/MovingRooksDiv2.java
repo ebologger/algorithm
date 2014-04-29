5922b9c07f2455130d26bfdd98f8d7f7860d0ee3
Enkhbold Tsagaach <etsagaach@medexpert.com>
2014-04-29 15:04:44 -0700

    SRM618 Div2 /complete/

--- topcoder_srm/618_Div2/MovingRooksDiv2.java
+++ topcoder_srm/618_Div2/MovingRooksDiv2.java
@@ -1,49 +1,47 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 //Not finished
 public class MovingRooksDiv2 {
 
 	public String move(int[] Y1, int[] Y2) {
 		int N = Y1.length;
-		int[] distance = new int[N];
 		
-		for(int i=0; i<N; i++){
-			for(int j=0; j<N; j++){
-				if(Y1[i] == Y2[j])
-					distance[Y1[i]] = j - i;
-			}
+		Set<String> visited = new HashSet<>();
+		
+		if(checkPossible(N, Y1, Y2, visited)){
+			return "Possible";
 		}
 		
-		if (!checkValidDistance(N, distance))
-			return "Impossible";
+		return "Impossible";
+	}
+	
+	public boolean checkPossible(int N, int[] Y1, int[] Y2, Set<String> visited){
 		
-		while(!checkCompleted(N, distance)){
-			int tmp = 0;
-			for(int i=0; i<N; i++){
-				tmp += distance[i];
-				if(tmp > 0){
-					
+		if(Arrays.equals(Y1, Y2))
+			return true;
+		
+		visited.add(Arrays.toString(Y1));
+		
+		System.out.println(Arrays.toString(Y1));
+				
+		for(int i=0;i<N-1;i++){
+			for(int j=i+1; j<N;j++){
+				if(Y1[i] > Y1[j]){
+					int[] Y1_Copy = Arrays.copyOf(Y1, N);
+					int tmp = Y1_Copy[i];
+					Y1_Copy[i] = Y1_Copy[j];
+					Y1_Copy[j] = tmp;
+					if(!visited.contains(Arrays.toString(Y1_Copy)))
+						if(checkPossible(N,Y1_Copy,Y2, visited))
+							return true;
 				}
 			}
 		}
 		
-		return "Possible";
+		
+		return false;
 	}
 	
-	public boolean checkValidDistance(int N, int[] distance){
-		int tmp = 0;
-		for(int i=N-1; i>=0; i--){
-			tmp += distance[i];
-			if(tmp < 0)
-				return false;			
-		}
-		return true;
-	}
-	
-	public boolean checkCompleted(int N, int[] distance){
-		for(int i=0; i<N; i++){
-			if(distance[i] != 0)
-				return false;			
-		}
-		return true;
-	}
-
 }
